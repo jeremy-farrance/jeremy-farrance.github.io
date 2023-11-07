90070b9f8515688f517f41d45483f203d3fd32a1
Jeremy Farrance <jfarrance@accuraty.com>
2023-11-07 14:08:46 -0600

    Another redo without new theme

--- AccuLadder/public/main.js
+++ AccuLadder/public/main.js
@@ -1,16 +1,1 @@
-/* file: accu/public/main.js */
-export default {
-  defaultTheme: 'dark',
-  iconLinks: [
-    {
-      icon: 'github',
-      href: 'https://github.com/Accuraty',
-      title: 'Accuraty'
-    },
-    {
-      icon: 'instagram',
-      href: 'https://instagram.com/Accuraty',
-      title: 'Instagram'
-    }
-  ]
-}
+export default {}
